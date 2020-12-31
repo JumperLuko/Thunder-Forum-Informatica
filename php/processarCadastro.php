d67822f9d7d2786f692441eb12530cd8cc377b68
Jumper Luko <jumper.luko@gmail.com>
2020-12-31 18:07:23 -0300

    2015-11-08_Backup_V2.30_visual_geral_e_cadastro

--- php/processarCadastro.php
+++ php/processarCadastro.php
@@ -1,43 +1,7 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        //recebe os dados preenchidos no formulário
-        $name = $_POST["name"];
-        $password = $_POST["password"];
-        $nickname = $_POST["nickname"];
-        $email = $_POST["email"];
-        //criptografa a senha
-        $password = md5($password);
-        //abrir conexão com o banco de dados
-        $conexao = mysqli_connect("localhost", "root", "root", "teste");
-        //Se a conexão foi aberta com sucesso, manda o comando. Se não foi, mostra qual erro ocorreu
-        if ($conexao == false) {
-            echo("Erro de conexão com o banco de dados. Entre em contato com o administrador.");
-            $erro = mysqli_connect_error($conexao);
-            echo ($erro);
-        } else {
-            //o comando é de inserir o novo usuário no banco
-            $comando = mysqli_query($conexao, "INSERT INTO usuarios (name, password, nickname, email) VALUES ('$name','$password', '$nickname', '$email')");
-            if ($comando == false) {
-                echo ("erro ao enviar comando para o banco de dados.");
-                $erro = mysqli_error($conexao);
-                echo($erro);
-            }else{
-                //caso o comando não retorne false, significa que deu certo.
-                echo("usuário cadastrado com sucesso.");
-            }
-        }
-        ?>
-        <a href="../index-homework.html">Voltar</a>
-    </body>
-</html>
+<meta http-equiv="Content-Type" content="text/html, charset=utf-8">
+
+<?php
+
+$con = mysqli_connect("localhost","root","root") or die("Não foi possível conectar com o servidor de dados!")
+
+?>
