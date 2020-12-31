41a8c9e6727f00ad18af9210f8096f8c9208d0a2
Jumper Luko <jumper.luko@gmail.com>
2020-12-31 17:37:30 -0300

    2015-08-10 Backup V1.51 visual pesquisa ok

--- php/cadastroImagem.php
+++
@@ -1,5 +0,0 @@
-<form action="cadastrarImagem.php" method="post" >
-    Jogo: <input type="text" name="jogo" required/><br/>
-    link: <input type="text" name="link" required/><br/>
-    <input type="submit" />
-</form>
