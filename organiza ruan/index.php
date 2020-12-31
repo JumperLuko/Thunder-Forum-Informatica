4f9ad99d47745f76f4a157724f80dcf515c04ce3
Jumper Luko <jumper.luko@gmail.com>
2020-12-31 17:34:53 -0300

    2015-05-04 Backup V0.001 Reconstrucao do topo incompleta

---
+++ organiza ruan/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt=br">
+    <head>
+        <title>Thunder</title>
+        <link rel="stylesheet" type="text/css" href="css/layout.css" />
+        <link rel="stylesheet" type="text/css" href="css/menu.css" />
+    </head>
+    <body>
+        <!-------------------------------- Topo -------------------------------->
+        <div id="topo">
+            <div id="topo_imagem"> <img src="img/logo2.png" width="20%" alt="Logo superior" /></div>
+        </div>
+
+        <!-------------------------------- Menu -------------------------------->
+        <div id="menu">            
+            <ul>
+                <li class='last'><a href='index.php'><span>Home<img src=""/></span></a></li>       
+                <li class='last'><a href='php/tutoriais.php'><span>Matérias</span></a></li>
+                <li class='last'><a href='php/cadastro.php'><span>Cadastro</span></a></li>
+            </ul>
+        </div>
+        <div id="corpo">
+                <div id="box1"> 
+                    <h2 id="titulo">Bem vindos ao Thunder search, onde sua duvida se transforma em conhecimento!</h2> <br>
+                    <p>Aprenda a escolher qual o melhor hardware para seu computador.
+                        <a href='tutoriais.php'><img src="img/extras/gabinete_pc.jpg" width="50%" alt="Gamer teste" /></a>
+                    </p>
+                </div>
+            </div>
+    </body>
+</html>
